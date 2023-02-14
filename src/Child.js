501fce09741450b7c4e86e72a8d16759e3d7a0cd
R S Sudir Krishnaa <rssudir.krishnaa@publicissapient.com>
2023-02-14 12:53:38 +0530

    Added Child Component

---
+++ src/Child.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Child = ({ onStateChange }) => {
+  const [state, setState] = useState("");
+
+// If you want to update the parent state whenever child state changes in real-time : Use Line 7 
+    onStateChange(state);
+
+
+  return (
+    <>
+        <h3>Contents from Child Component</h3>
+        <input type="text" value={state} onChange={(e) => setState(e.target.value)} />
+        {/* If you want to update the parent state whenever child state changes on a button click use onClick() like below */}
+        <button type="submit" onClick={ () =>  onStateChange(state)}>Submit</button>
+    </>
+    
+  );
+};
+
+export default Child;
